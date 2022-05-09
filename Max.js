b85b974b1479204c3da1a715d268be9c35c9d1e7
KennyRandria <103984550+KennyRandria@users.noreply.github.com>
2022-05-09 10:49:53 +0300

    Update Max.js

--- Max.js
+++ Max.js
@@ -1,7 +1,7 @@
 function getMax(t){
     let max;
     for(let index = 0; index< t.length; index++){
-        if(t[index]<max && index == 0){
+        if(t[index]<max){
             max=t[index];
         }
         if(t[index]>max)
